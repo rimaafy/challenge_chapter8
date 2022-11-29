276b753741fb6e15c5a3dcc03f4fd87feeca073b
rimaafy <rimafya17@gmail.com>
2022-11-29 22:26:50 +0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const {
   DB_USER = "",
   DB_PASSWORD = "",
-  DB_NAME = "challenge_chapter8",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
+  DB_NAME = "",
+  DB_HOST = "",
+  DB_PORT = "",
 } = process.env;
 
 // process.env.development = local 
